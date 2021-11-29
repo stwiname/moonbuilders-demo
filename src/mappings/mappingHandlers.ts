dee0759875aa71d5add4956fed8f1fc0e7c88e78
Scott Twiname <skott.twiname@gmail.com>
2021-11-29 17:21:13 +1300

    Add demo

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,36 @@
+import {SubstrateEvent} from "@subql/types";
+import {Erc20Transfer, Collator} from "../types";
+import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
+import { BigNumber } from '@ethersproject/bignumber';
+
+export async function collatorJoined(event: SubstrateEvent): Promise<void> {
+
+    const address = event.extrinsic.extrinsic.signer.toString();
+
+    const collator = Collator.create({
+        id: address,
+        joinedDate: event.block.timestamp
+    });
+
+    await collator.save();
+
+}
+
+export async function collatorLeft(event: SubstrateEvent): Promise<void> {
+
+    const address = event.extrinsic.extrinsic.signer.toString();
+    Collator.remove(address);
+}
+
+export async function erc20Transfer(event: MoonbeamEvent<[string, string, BigNumber] & { from: string, to: string, value: BigNumber, }>): Promise<void> {
+    const transfer = Erc20Transfer.create({
+        id: event.transactionHash,
+        from: event.args.from,
+        to: event.args.to,
+        amount: event.args.value.toBigInt(),
+    });
+
+    await transfer.save();
+}
+
+
